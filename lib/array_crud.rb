73b08f4bd47c5c006c1e74fffbf4b048c8793b7b
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-11-26 22:25:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array (array, element)
  array.unshift(element)
  end
  
- def remove_element_from_end_of_array(array)
-   array.pop(-1)
+ def remove_element_from_end_of_array(element)
+   array.pop(0)
  end
  
